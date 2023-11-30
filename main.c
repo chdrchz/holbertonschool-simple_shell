67c0db291ad0dfd10d39263304637417608733a8
Greentaylor27 <7196@holbertonstudents.com>
2023-11-30 09:42:32 -0800

    Pushing 11/30 11:42am adding tokenization

--- main.c
+++ main.c
@@ -1,41 +1,58 @@
 #include "main.h"
 
-char *_strtok(char *str, const char *delim __attribute__((unused)))
+int main(int ac, char **av)
 {
-	char *token = strtok(str, " ");
-	delim = "";
-	
-	while (token != NULL)
-	{
-		strtok(NULL, " ");
-		token++;
-	}
-	return (token);
-}
-
-int main(int ac __attribute__((unused)), char **av)
-{
-	char *buffer = NULL;
-	char *cmdBuffer = NULL;
+	char *input, *tokenStr, *token = NULL;
 	size_t *bufferSize = 0;
-	
-	cmdBuffer = malloc(1024 * sizeof(char));
+	char *prompt = "$ ";
+	int numToken = 0, count = 0, i = 0;
+	ssize_t storedInput = 0;
+	(void)ac;
+	tokenStr = malloc(sizeof(char) * storedInput);
 	bufferSize = malloc(sizeof(size_t));
-
+	av = malloc(sizeof(char) * numToken);
 	if (bufferSize == NULL)
+	{
 		free(bufferSize);
-
+		return (-1);
+	}
+	if (tokenStr == NULL)
+	{
+		free(tokenStr);
+		return (-1);
+	}
 	while (1)
 	{
-		printf("$ ");
-		getline(&buffer, bufferSize, stdin);
-		sprintf(cmdBuffer, "which %s", buffer);
-		system(cmdBuffer);
-		while (*av != NULL)
+		printf("%s", prompt);
+		storedInput = getline(&input, bufferSize, stdin);
+		if (storedInput == -1)
 		{
-			printf("%s\n", *av);
-			av++;
+			printf("Thanks for stopping by...\n");
+			return (-1);
 		}
+		strcpy(tokenStr, input);
+		token = strtok(tokenStr, " ");
+		while (token != NULL)
+		{
+			numToken++;
+			token = strtok(NULL, " ");
+		}
+		numToken++;
+		token = strtok(tokenStr, " ");
+		while (token != NULL)
+		{
+			count++;
+			av[count] = malloc(sizeof(char) * strlen(token));
+			strcpy(av[count], token);
+			token = strtok(NULL, " ");
+		}
+		/* loop for testing purposes only */
+		for (i = 0; i < numToken - 1; i++)
+			printf("%s", av[count]);
+		av[count] = NULL;
+		free(tokenStr);
+		free(bufferSize);
+		free(av);
 	}
 	return (0);
 }
